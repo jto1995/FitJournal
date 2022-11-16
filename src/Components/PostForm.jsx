3d41560599d0bc77a0701683cd49e3e26a2f41f5
jto1995 <to67@hotmail.com>
2022-11-16 10:51:48 -0800

    fixed form

--- src/Components/PostForm.jsx
+++ src/Components/PostForm.jsx
@@ -7,6 +7,7 @@ export default function PostForm (props) {
         <section>
             <p className="mb-2">What are you logging?</p>
             <select className="mb-2" onSelect={handleSelect} name="selectList" id="selectList">
+                <option value="" disabled selected>Options</option>
                 <option value="option 1">{props.option1}</option>
                 <option value="option 2">{props.option2}</option>
                 <option value="option 3">{props.option3}</option>
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -11,29 +11,64 @@ export default function PostPage() {
     const [exerciseInput, setExerciseInput] = useState([
         { sets:'', reps:'', exercise:''}
     ]);
+    const [weightInput, setWeightInput] = useState([
+        { weight:''}
+    ])
+    const handleWeightChange = (index, e) => {
+        let data = [weightInput];
+        data[index][e.target.weight] = e.target.value;
+        setWeightInput(data)
+    }
 
-    const handleFoodChange = (index, event) => {
-        let data = [...foodInput];
-        data[index][event.target.foodEntry] = event.target.value;
+    const handleFoodChange = (index, e) => {
+        let data = [foodInput];
+        data[index][e.target.foodEntry] = e.target.value;
         setFoodInput(data)
     };
 
-    const handleExerciseChange = (index, event) => {
-        let data = [...exerciseInput]
-        data[index][event.target.sets] = event.target.value;
+    const handleExerciseChange = (index, e) => {
+        let data = [exerciseInput]
+        data[index][e.target.sets] = e.target.value;
         setExerciseInput(data)
     }; 
+    
+    const handleExerciseSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const handleFoodSubmit = (e) => {
+        e.preventDefault()
+    }
+    const handleWeightSubmit = (e) => {
+        e.preventDefault()
+    }
 
     return (
         <div>
             <UserHeader/>
             <div className="bg-gradient-to-r from-green-100 to-sky-300 p-4 h-screen">
-                <PostForm option1="Options" option2="Exercise" option3="Food"/>
+                <PostForm option1="Weight" option2="Exercise" option3="Food"/>
                 <div>
-                    <h2 className="mb-2 font-bold">Logging Food</h2>
+                    <h2>Weight Log</h2>
+                    <form onSubmit={handleWeightSubmit}>
+                        {weightInput.map((input, index) => {
+                            return(
+                        <input 
+                            type="number"
+                            placeholder="Weight in lbs"
+                            value={input.weight}
+                            onChange={e => handleWeightChange(index, e)}
+                        />
+                        )},
+                        )}
+                        <Btn btnText='Submit'/>
+                    </form>
+                </div>
+                <div>
+                    <h2 className="mb-2 font-bold">Food Log</h2>
                     <p className="italic mb-2">Here you're able to log what you ate today and it will submit to our API to calculate to track your nutrition and store it into our server for your to keep track of! You will be able to see if you're hitting your macros each day!</p>
                     <div >
-                        <form className="flex flex-col">
+                        <form onSubmit={handleFoodSubmit} className="flex flex-col">
                             {foodInput.map((input, index) => {
                                 return (
                                     <div className="flex flex-col"key={index}>
@@ -43,7 +78,7 @@ export default function PostPage() {
                                         name='food Entry'
                                         placeholder='Food Item'
                                         value={input.food}
-                                        onChange={event => handleFoodChange(index, event)}
+                                        onChange={e => handleFoodChange(index, e)}
                                     />
                                     </div>
                                 )
@@ -62,7 +97,7 @@ export default function PostPage() {
                 <div className="flex flex-col justify-center">
                     <PostForm option1="Upper body" option2="Lower body" option3="Cardio & Abs"/>
                     <div>
-                        <form className="flex flex-col">
+                        <form onSubmit={handleExerciseSubmit} className="flex flex-col">
                         {exerciseInput.map((input, index) => {
                                 return (
                                     <div className="flex flex-col mb-6 "key={index}>
@@ -71,21 +106,23 @@ export default function PostPage() {
                                             name='exercise'
                                             placeholder='Exercise'
                                             value={input.exercise}
-                                            onChange={event => handleExerciseChange(index, event)}
+                                            onChange={e => handleExerciseChange(index, e)}
                                         />
                                         <input
+                                            type='number'
                                             className='mb-6 py-1 pl-2 italic rounded-xl'
                                             name='sets'
                                             placeholder='Sets'
                                             value={input.sets}
-                                            onChange={event => handleExerciseChange(index, event)}
+                                            onChange={e => handleExerciseChange(index, e)}
                                         />
                                         <input
+                                            type='number'
                                             className='mb-6 py-1 pl-2 italic rounded-xl'
                                             name='reps'
                                             placeholder='Reps'
                                             value={input.reps}
-                                            onChange={event => handleExerciseChange(index, event)}
+                                            onChange={e => handleExerciseChange(index, e)}
                                         />
                                         <div className="flex justify-evenly mt-4">
                                             <Btn btnText='+' />
